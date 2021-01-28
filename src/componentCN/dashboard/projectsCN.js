17270e977c13477c6d68c4f5a28fc45161344b8e
YueqingXie <43152856+YueqingXie@users.noreply.github.com>
2021-01-28 15:01:51 -0500

    Update projectsCN.js

--- src/componentCN/dashboard/projectsCN.js
+++ src/componentCN/dashboard/projectsCN.js
@@ -70,6 +70,19 @@ class projectsCN extends Component {
             <section className="container section">
                 <h4 className="titleENG center">YX-我的项目</h4>
 
+            
+            <div className="YX-Ps">
+                    <div className="row">
+                        <div className="col s12 m6 l6">
+                            <p>YX-P6 (博客平台): </p>                            
+                            <p className="grey-text">[Vue&Firebase] 2021/01/28 - 2021/01/28</p>
+                        </div>
+                        <div className="col s12 m6 l6">
+                            <a href="https://yxp6.netlify.app/" target="_blank" rel="noopener noreferrer">https://yxp6.netlify.app/</a>   
+                        </div>
+                    </div>
+                </div>
+        
             <div className="YX-Ps">
                     <div className="row">
                         <div className="col s12 m6 l6">
