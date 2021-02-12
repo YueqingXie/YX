374f8bdf5bca498385c6aae48ffeec247c9e1c50
YueqingXie <43152856+YueqingXie@users.noreply.github.com>
2021-02-12 00:55:43 -0500

    Update projects.js

--- src/component/dashboard/projects.js
+++ src/component/dashboard/projects.js
@@ -73,7 +73,7 @@ class projects extends Component {
               <div className="YX-Ps">
                     <div className="row">
                         <div className="col s12 m6 l6">
-                            <p>YX-P9 (Recipe): </p>
+                            <p>YX-P9 (Recipe App): </p>
                             <p className="grey-text">[PWA&Firebase] 2021/02/11 - 2021/02/11</p>
                         </div>
                         <div className="col s12 m6 l6">
