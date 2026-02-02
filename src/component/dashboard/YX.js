ee2470968a4ab6a482ead9566b51dd0fc28c3348
unknown <xieyueqingxyq@gmail.com>
2026-02-02 14:55:59 -0500

    height

--- src/component/dashboard/YX.js
+++ src/component/dashboard/YX.js
@@ -31,7 +31,7 @@ class YX extends Component {
                             <p className="YXdetailP"><strong>Education: </strong>Bachelor’s Degree in Joint Honours in Computer Science and Mathematics.</p>
                             <p className="YXdetailP"><strong>Birthday: </strong>January 26</p>
                             <p className="YXdetailP"><strong>MBTI: </strong>INTJ-A</p>
-                            <p className="YXdetailP"><strong>Height: </strong>168cm</p>
+                            <p className="YXdetailP"><strong>Height: </strong>169cm</p>
                             <p className="YXdetailP"><strong>Weight: </strong>50kg</p>
                         </div>
                     </div>
